e0bb0402d520deeb8ff335ed964903217c1e50d9
zikaloai71 <ze.lo.moukhtar@el-eng.menofia.edu.eg>
2024-06-05 17:34:36 +0300

    connect to cloud flare worker api

--- src/hooks/useTranslator.tsx
+++ src/hooks/useTranslator.tsx
@@ -1,24 +1,21 @@
-import OpenAI from "openai";
-
-
-
-const openai = new OpenAI({
-    dangerouslyAllowBrowser:true,
-    apiKey: import.meta.env.VITE_APP_OPEN_AI as string,
-  });
-  
 export const useTranslator = () => {
 
     async function translate(language:string,text:string) {
         try{
-            const completion = await openai.chat.completions.create({
-                messages: [{"role": "system", "content": "You are a translator who has knowledge in different languages such as arabic, english , french and spanish the user will ask you in english to translate a certain text to one of the language you speak fluently and you should reply with the translated text for this sentence only with no more words than the original text"},
-                    {"role": "user", "content": `Translate into ${language} the following text: ${text}`},
-                  ],
-                model: "gpt-3.5-turbo",
-              });
+            const resp = await fetch("https://openai-api-worker.zakarialoai71.workers.dev/",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify([{"role": "system", "content": "You are a translator who has knowledge in different languages such as arabic, english , french and spanish the user will ask you in english to translate a certain text to one of the language you speak fluently and you should reply with the translated text for this sentence only with no more words than the original text"},
+                {"role": "user", "content": `Translate into ${language} the following text: ${text}`},
+              ]),
+              }
+            )
+            const completion = await resp.json() as {content:string}
              
-            return completion.choices[0].message.content
+            return completion.content
         }
         catch(err){
             console.log(err)
